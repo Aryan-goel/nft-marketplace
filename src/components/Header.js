1e940a3453a46174d9b047147cf97c4152c94ddf
91639 <goel.aryan2406@gmail.com>
2022-01-13 20:27:42 +0530

    assets loaded

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,22 @@
-import React from 'react'
-import './Header.css'
-import punkLogo from '../assets/header/cryptopunk-logo.png'
+import React from "react";
+import "./Header.css";
+import punkLogo from "../assets/header/cryptopunk-logo.png";
+import searchIcon from "../assets/header/search.png";
 
 const Header = () => {
-    return (
-        <div className='header'>
-    <div className='logoContainer'>
-        <img src={punkLogo} classname='punk logo' alt=""/>
-    </div>
+  return (
+    <div className="header">
+      <div className="logoContainer">
+        <img src={punkLogo} className="punkLogo" alt="" />
+      </div>
+      <div className="searchBar">
+        <div className="searchIconContainer">
+          <img src={searchIcon} />
         </div>
-    )
-}
+        <input className="searchInput"  placeholder="collection items or user ....."/>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
