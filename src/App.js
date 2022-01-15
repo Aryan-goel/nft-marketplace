2a888d668591799697d1ef6645ee4c1bb715f6a9
91639 <goel.aryan2406@gmail.com>
2022-01-15 17:44:40 +0530

    creating collection cards

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+import CollectionCard from "./components/CollectionCard";
 
 function App() {
   return (
-    <div className='app'>
-   <Header/>
-   </div>
-    
+    <div className="app">
+      <Header />
+      <CollectionCard
+        id={0}
+        name={'aryan'}
+        traits={[{ value: 7 }]}
+        image="https://ipfs.thirdweb.com/ipfs/QmbxpMKvCQV9rGNujnWBr1cb41QnFk18UBb1uYEfJihy5y/0.jpg"
+      />
+    </div>
   );
 }
 
