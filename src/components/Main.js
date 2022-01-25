e69e69582bea31c235f8d350f20adbda863453cd
91639 <goel.aryan2406@gmail.com>
2022-01-25 16:05:47 +0530

    completed!!!!

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,13 +2,12 @@ import React, { useEffect, useState } from "react";
 import InstagramLogo from "../assets/owner/instagram.png";
 import moreIcon from "../assets/owner/more.png";
 import "./Main.css";
-const Main = ({selectedPunk,punkListData}) => {
-  const[activePunk,setActivePunk]=useState(punkListData[0])
+const Main = ({ selectedPunk, punkListData }) => {
+  const [activePunk, setActivePunk] = useState(punkListData[0]);
 
-
-  useEffect(()=>{
-    setActivePunk(punkListData[selectedPunk])
-  },[punkListData,selectedPunk])
+  useEffect(() => {
+    setActivePunk(punkListData[selectedPunk]);
+  }, [punkListData, selectedPunk]);
 
   return (
     <div className="main">
@@ -17,32 +16,22 @@ const Main = ({selectedPunk,punkListData}) => {
           <div className="punkContainer">
             <img
               className="selectedPunk"
-              src={
-                activePunk.image_original_url
-              }
+              src={activePunk.image_original_url}
               alt=""
             />
           </div>
         </div>
-        <div className="punkDetails" style={{ color: "#fff" }}>
-          <div className="title">
-            {/* {activePunk.name} */}
-            Zombie punk
-          </div>
-          <span className="itemNumber">#1</span>
+        <div className="punkDetails" style={{ color: '#fff' }}>
+          <div className="title">{activePunk.name}</div>
+          <span className="itemNumber">#{activePunk.token_id}</span>
         </div>
         <div className="owner">
           <div className="ownerImageContainer">
-            <img
-              src="https://ipfs.thirdweb.com/ipfs/QmbxpMKvCQV9rGNujnWBr1cb41QnFk18UBb1uYEfJihy5y/0.jpg"
-              alt=""
-            />
+            <img src={activePunk.owner.profile_img_url} alt="" />
           </div>
           <div className="ownerDetails">
             <div className="ownerNameAndHandle">
-              <div className="address">
-                0xe911396b58898D08bd1264f4746b40651026B932
-              </div>
+              <div className="address">{activePunk.owner.address}</div>
               <div className="ownerHandle">@AryanGoel</div>
             </div>
             <div className="ownerLink">
